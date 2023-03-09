768ed5808a3168bf42bc8f425cc3645bcb45c183
Olaf Mihari <olafnjenga@gmail.com>
2023-03-09 06:58:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,67 @@
+function add(x,y){
+    return x+y;
+}
+let x = subtract (3, 2)  //function is called, return value will end up in x
+
+function subtract(a,b){
+    return a-b;  //function returns the product of a and b
+}
+
+function multiply(a,b){
+    return a*b;  
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+ let n=2;
+while (n <= 10){
+    console.log(n);
+    counter = counter +1;
+}
+
+var i = 0;
+
+function increament(n){
+    n++;
+    return n;
+}
+
+i=increament(i);
+
+var d = 0
+
+function decrement(n){
+    n--;
+    return n;
+}
+
+d=decrement(d);
+
+//parsing
+
+function makeInt(){
+
+    (parseInt(n, 10));
+}
+makeInt();
+
+function makeInt(n) {
+
+    return (parseInt(n, 10));
+}
+makeInt("2")
+
+//return NaN
+
+function preserveDecimal(n){
+    (parseInt("", 10));
+}
+
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+preserveDecimal(3.14);
